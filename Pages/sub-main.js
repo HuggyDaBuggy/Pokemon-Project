e367b4d714796169265c1444bba8d84e45dcf8dd
HuggyDaBuggy <hugo@maxtedfamily.net>
2020-09-01 10:01:59 +0800

    Pokemon

--- Pages/sub-main.js
+++ Pages/sub-main.js
@@ -5,7 +5,14 @@ function login() {
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
 	if (username == AdminUsername && password == AdminPassword) {
-		
+		alert("Login successful but am to lazy too finish the code")
+	}
+	else  if (username == ""){
+		alert("Please input a Username")
+	}
+	else  if (password == ""){
+		alert("Please input a Password")
+	
 	}
 	else {
 		alert("Incorrect Username/Password")
