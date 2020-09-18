43770cfaf391b7fd62219cc36fa8c343bb840644
Hugo Maxted <hugo@maxtedfamily.net>
2020-09-18 14:57:51 +0800

    dumb dumb

--- Pages/sub-main.js
+++ Pages/sub-main.js
@@ -1,23 +1,41 @@
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 var AdminUsername = "HuggyDaBuggy";
 var AdminPassword = "buggy";
 
 function login() {
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
-	if (username == AdminUsername && password == AdminPassword) {
-		alert("Login successful but am to lazy too finish the code")
-	}
-	else  if (username == ""){
-		alert("Please input a Username")
-	}
-	else  if (password == ""){
-		alert("Please input a Password")
-	}
-	else {
-		alert("Incorrect Username/Password")
-	}
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if (username == AdminUsername && password == AdminPassword) {
+    setCookie("admin", "logged-in", 365);
+    window.location.replace("admin.html");
+  } else if (username == "") {
+    alert("Please input a Username");
+  } else if (password == "") {
+    alert("Please input a Password");
+  } else {
+    alert("Incorrect Username/Password");
+  }
 }
 
-function load() {
-
-}
+function load() {}
