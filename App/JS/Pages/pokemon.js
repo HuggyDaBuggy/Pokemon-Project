c2ac734b216b9c76d93cf283336c777555d0904a
HuggyDaBuggy <hugo@maxtedfamily.net>
2020-10-03 14:18:54 +0800

    Pretty

--- App/JS/Pages/pokemon.js
+++ App/JS/Pages/pokemon.js
@@ -1,6 +1,6 @@
 var url = window.location.href;
 var urlsplit = url.split("?");
-var pokemon = urlsplit[1]
+var pokemon = urlsplit[1];
 if (pokemon == null) {
-  window.location.replace("index.html")
+  window.location.replace("index.html");
 }
