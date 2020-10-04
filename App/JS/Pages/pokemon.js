75791b27d069b4c7ad249ef4a53841ad71850b7d
HuggyDaBuggy <hugo@maxtedfamily.net>
2020-10-04 15:18:47 +0800

    Yum stuff

--- App/JS/Pages/pokemon.js
+++ App/JS/Pages/pokemon.js
@@ -1,45 +1,60 @@
-var pokemons = {
-  Pikachu: {type: "Cute", id: "025", number: 1, info: ""},
-  Eevee: {type: "Cute", id: "133", number: 2, info: ""},
-  Mew: {type: "Cute", id: "151", number: 3, info: ""},
-  Togepi: {type: "Cute", id: "175", number: 4, info: ""},
-  Skitty: {type: "Cute", id: "300", number: 5, info: ""},
-  Vulpix: {type: "Cute", id: "037", number: 6, info: ""},
-  Emolga: {type: "Cute", id: "587", number: 7, info: ""},
-  Deerling: {type: "Cute", id: "585", number: 8, info: ""},
-  Snom: {type: "Cute", id: "872", number: 9, info: ""},
-  Teddiursa: {type: "Cute", id: "216", number: 10, info: ""},
-  Jirachi: {type: "Cute", id: "385", number: 11, info: ""},
-  Jigglypuff: {type: "Cute", id: "039", number: 12, info: ""},
-  Shaymin: {type: "Cute", id: "492", number: 13, info: ""},
-  Meloetta: {type: "Cute", id: "648", number: 14, info: ""},
-  Absol: {type: "Cute", id: "359", number: 15, info: ""},
-  Jynx: {type: "Ugly", id: "124", number: 1, info: ""},
-  Probopass: {type: "Ugly", id: "476", number: 2, info: ""},
-  Garbodor: {type: "Ugly", id: "569", number: 3, info: ""},
-  Conkelgurr: {type: "Ugly", id: "534", number: 4, info: ""},
-  Barbaracla: {type: "Ugly", id: "689", number: 5, info: ""},
-  Stunfisk: {type: "Ugly", id: "618", number: 6, info: ""},
-  Seismitoad: {type: "Ugly", id: "537", number: 7, info: ""},
-  Purugly: {type: "Ugly", id: "432", number: 8, info: ""},
-  Muk: {type: "Ugly", id: "089", number: 9, info: ""},
-  Drowzee: {type: "Ugly", id: "096", number: 10, info: ""},
-  Swampert: {type: "Ugly", id: "260", number: 11, info: ""},
-  Walrein: {type: "Ugly", id: "365", number: 12, info: ""},
-  Malmar: {type: "Ugly", id: "687", number: 13, info: ""},
-  Noivern: {type: "Ugly", id: "715", number: 14, info: ""},
-  Skuntank: {type: "Ugly", id: "435", number: 15, info: ""},
-};
+var cutepokemon = [
+  ["Pikachu", "025", 1, ""],
+  ["Eevee", "133", 2, ""],
+  ["Mew", "151", 3, ""],
+  ["Togepi", "175", 4, ""],
+  ["Skitty", "300", 5, ""],
+  ["Vulpix", "037", 6, ""],
+  ["Emolga", "587", 7, ""],
+  ["Deerling", "585", 8, ""],
+  ["Snom", "872", 9, ""],
+  ["Teddiursa", "216", 10, ""],
+  ["Jirachi", "385", 11, ""],
+  ["jigglypuff", "039", 12, ""],
+  ["Shaymin", "492", 13, ""],
+  ["Meloetta", "648", 14, ""],
+  ["Absol", "359", 15, ""],
+];
+var uglypokemon = [
+  ["Jynx", "124", 1, ""],
+  ["Probopass", "476", 2, ""],
+  ["Garbodor", "569", 3, ""],
+  ["Conkelgurr", "534", 4, ""],
+  ["Barbaracla", "689", 5, ""],
+  ["Stunfisk", "618", 6, ""],
+  ["Seismitoad", "537", 7, ""],
+  ["Purugly", "432", 8, ""],
+  ["Muk", "089", 9, ""],
+  ["Drowzee", "096", 10, ""],
+  ["Swampert", "260", 11, ""],
+  ["Walrein", "365", 12, ""],
+  ["Malmar", "687", 13, ""],
+  ["Noivern", "715", 14, ""],
+  ["Skuntank", "435", 15, ""],
+];
 var url = window.location.href;
 var urlsplit = url.split("?");
 var pokemon = urlsplit[1];
-if (pokemon == null) {
-  window.location.replace("index");
+var i = 0;
+var type = "";
+while (i < 15) {
+  if (cutepokemon[i][0] == pokemon) {
+    var type = "Cute";
+    alert(cutepokemon[i][0]);
+  }
+  if (uglypokemon[i][0] == pokemon) {
+    var type = "Cute";
+    alert(uglypokemon[i][0]);
+  }
+  var i = i + 1
+}
+if (type == "") {
+  window.location.replace("index")
 }
 document.title = "Pokemon - " + pokemon;
 document.getElementById("pokemon-name").innerHTML = pokemon;
 document.getElementById("pokemon-info").innerHTML = pokemons[pokemon]["info"];
-document.getElementById("pokemon-image").setAttribute("src", "App/Images/Pokemon/" + pokemons[pokemon]["type"] + "/" + pokemons[pokemon]["id"] + ".png");
+document.getElementById("pokemon-image").setAttribute("src", "App/Images/Pokemon/" + type + "/" + pokemons[pokemon]["id"] + ".png");
 document.getElementById("pokemon-image").setAttribute("alt", pokemon);
 document.getElementById("related-pokemon-image-1").setAttribute("src", "");
 document.getElementById("related-pokemon-image-1").setAttribute("alt", "");
