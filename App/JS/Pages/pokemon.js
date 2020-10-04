2534960f4e7836667e9647a414758b85e8624d6d
HuggyDaBuggy <hugo@maxtedfamily.net>
2020-10-04 10:14:40 +0800

    Stuff

--- App/JS/Pages/pokemon.js
+++ App/JS/Pages/pokemon.js
@@ -4,3 +4,17 @@ var pokemon = urlsplit[1];
 if (pokemon == null) {
   window.location.replace("index");
 }
+document.getElementById("pokemon-name").innerHTML = pokemon;
+document.getElementById("pokemon-info").innerHTML = pokemon;
+document.getElementById("related-pokemon-image-1").setAttribute("src", "");
+document.getElementById("related-pokemon-image-1").setAttribute("alt", "");
+document.getElementById("related-pokemon-1").innerHTML = "";
+document.getElementById("related-pokemon-image-2").setAttribute("src", "");
+document.getElementById("related-pokemon-image-2").setAttribute("alt", "");
+document.getElementById("related-pokemon-2").innerHTML = "";
+document.getElementById("related-pokemon-image-3").setAttribute("src", "");
+document.getElementById("related-pokemon-image-3").setAttribute("alt", "");
+document.getElementById("related-pokemon-3").innerHTML = "";
+document.getElementById("related-pokemon-image-4").setAttribute("src", "");
+document.getElementById("related-pokemon-image-4").setAttribute("alt", "");
+document.getElementById("related-pokemon-4").innerHTML = "";
