e46701b002e619b4442fec5c6903676d9704d735
HuggyDaBuggy <hugo@maxtedfamily.net>
2020-10-05 16:15:45 +0800

    Custom context menu

---
+++ App/JS/main.js
@@ -0,0 +1,19 @@
+document.addEventListener(
+  "contextmenu",
+  function (rclick) {
+    rclick.preventDefault();
+  },
+  false
+);
+document.addEventListener("contextmenu", contextMenu);
+window.onclick = function () {
+  document.getElementById("context-menu").style.display = "none";
+};
+window.onscroll = function () {
+  document.getElementById("context-menu").style.display = "none";
+};
+function contextMenu(event) {
+  document.getElementById("context-menu").style.left = event.clientX + "px";
+  document.getElementById("context-menu").style.top = event.clientY + "px";
+  document.getElementById("context-menu").style.display = "block";
+}
