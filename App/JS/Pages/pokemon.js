166cadccd254b1617a001f8a19792f693aa6dc4f
HuggyDaBuggy <hugo@maxtedfamily.net>
2020-10-06 07:31:14 +0800

    Slide show stuff

--- App/JS/Pages/pokemon.js
+++ App/JS/Pages/pokemon.js
@@ -31,38 +31,38 @@ var pokemons = [
   "Skuntank",
 ];
 var cutepokemon = [
-  ["Pikachu", "025", "The electic mouse pokémon. It stores electricity in it's cheeks and attacks with energy equivalent to a lighting bolt.", "", "3"],
-  ["Eevee", "133", "The evolution pokémon. Eevee is most well known for the largest amount of possible evolutions - 8 in total.", "", "9"],
-  ["Mew", "151", "The new species pokémon. This mythical creature possesses the genetic information of every Pokémon and is rarely seen.", "", "1"],
-  ["Togepi", "175", "The spike ball pokémon. It's shell is said to contain happiness and Togepi often shares it with others of pure intent.", "", "3"],
-  ["Skitty", "300", "The kitten pokémon. Skitty likes to chase it's own tail until it falls dizzy. It is very popular as a pet.", "", "2"],
-  ["Vulpix", "037", "The fox pokémon. It is able to control a flame in it's mouth that never goes out. As it grows, it's tails split to make more.", "", "2"],
-  ["Emolga", "587", "", "", "1"],
-  ["Deerling", "585", "", "", "2"],
-  ["Snom", "872", "", "", "2"],
-  ["Teddiursa", "216", "", "", "2"],
-  ["Jirachi", "385", "", "", "1"],
-  ["jigglypuff", "039", "", "", "3"],
-  ["Shaymin", "492", "", "", "1"],
-  ["Meloetta", "648", "", "", "1"],
-  ["Absol", "359", "", "", "1"],
+  ["Pikachu", "025", "The electic mouse pokémon. It stores electricity in it's cheeks and attacks with energy equivalent to a lighting bolt.", "", 3],
+  ["Eevee", "133", "The evolution pokémon. Eevee is most well known for the largest amount of possible evolutions - 8 in total.", "", 2],
+  ["Mew", "151", "The new species pokémon. This mythical creature possesses the genetic information of every Pokémon and is rarely seen.", "", 1],
+  ["Togepi", "175", "The spike ball pokémon. It's shell is said to contain happiness and Togepi often shares it with others of pure intent.", "", 3],
+  ["Skitty", "300", "The kitten pokémon. Skitty likes to chase it's own tail until it falls dizzy. It is very popular as a pet.", "", 2],
+  ["Vulpix", "037", "The fox pokémon. It is able to control a flame in it's mouth that never goes out. As it grows, it's tails split to make more.", "", 2],
+  ["Emolga", "587", "", "", 1],
+  ["Deerling", "585", "", "", 2],
+  ["Snom", "872", "", "", 2],
+  ["Teddiursa", "216", "", "", 2],
+  ["Jirachi", "385", "", "", 1],
+  ["jigglypuff", "039", "", "", 3],
+  ["Shaymin", "492", "", "", 1],
+  ["Meloetta", "648", "", "", 1],
+  ["Absol", "359", "", "", 1],
 ];
 var uglypokemon = [
-  ["Jynx", "124", "", "", "2"],
-  ["Probopass", "476", "", "", "2"],
-  ["Garbodor", "569", "", "", "2"],
-  ["Conkeldurr", "534", "", "", "3"],
-  ["Barbaracle", "689", "", "", "2"],
-  ["Stunfisk", "618", "", "", "1"],
-  ["Seismitoad", "537", "", "", "3"],
-  ["Purugly", "432", "", "", "2"],
-  ["Muk", "089", "", "", "2"],
-  ["Drowzee", "096", "", "", "2"],
-  ["Swampert", "260", "", "", "3"],
-  ["Walrein", "365", "", "", "3"],
-  ["Malamar", "687", "", "", "2"],
-  ["Noivern", "715", "", "", "2"],
-  ["Skuntank", "435", "", "", "2"],
+  ["Jynx", "124", "", "", 2],
+  ["Probopass", "476", "", "", 2],
+  ["Garbodor", "569", "", "", 2],
+  ["Conkeldurr", "534", "", "", 3],
+  ["Barbaracle", "689", "", "", 2],
+  ["Stunfisk", "618", "", "", 1],
+  ["Seismitoad", "537", "", "", 3],
+  ["Purugly", "432", "", "", 2],
+  ["Muk", "089", "", "", 2],
+  ["Drowzee", "096", "", "", 2],
+  ["Swampert", "260", "", "", 3],
+  ["Walrein", "365", "", "", 3],
+  ["Malamar", "687", "", "", 2],
+  ["Noivern", "715", "", "", 2],
+  ["Skuntank", "435", "", "", 2],
 ];
 var pokemon = window.location.href.split("?")[1];
 var a = 0;
@@ -87,8 +87,12 @@ while (a < 15) {
     document.getElementById("related-pokemon").innerHTML = "Other Cute Pokemon";
     document.getElementById("pokemon-info").innerHTML = cutepokemon[a][2];
     document.getElementById("pokemon-stats").innerHTML = cutepokemon[a][3];
-    document.getElementById("pokemon-image").setAttribute("src", "App/Images/Pokemon/Cute/" + cutepokemon[a][1] + ".png");
-    document.getElementById("pokemon-image").setAttribute("alt", pokemon);
+    document.getElementById("pokemon-image-1").setAttribute("src", "App/Images/Pokemon/Cute/" + cutepokemon[a][1] + ".png");
+    document.getElementById("pokemon-image-1").setAttribute("alt", pokemon);
+    if (cutepokemon[a][4] == 1) {
+      document.getElementById("pokemon-controls").style.display = "none";
+      document.getElementById("pokemon-image-text-1").style.display = "none";
+    }
     while (b < 4) {
       if (b < 2) {
         var bb = a - 2 + b;
@@ -118,8 +122,11 @@ while (a < 15) {
     document.getElementById("related-pokemon").innerHTML = "Other Ugly Pokemon";
     document.getElementById("pokemon-info").innerHTML = uglypokemon[a][2];
     document.getElementById("pokemon-stats").innerHTML = uglypokemon[a][3];
-    document.getElementById("pokemon-image").setAttribute("src", "App/Images/Pokemon/Ugly/" + uglypokemon[a][1] + ".png");
-    document.getElementById("pokemon-image").setAttribute("alt", pokemon);
+    document.getElementById("pokemon-image-1").setAttribute("src", "App/Images/Pokemon/Ugly/" + uglypokemon[a][1] + ".png");
+    document.getElementById("pokemon-image-1").setAttribute("alt", pokemon);
+    if (uglypokemon[a][4] == 1) {
+      document.getElementById("pokemon-controls").style.display = "none";
+    }
     while (c < 4) {
       if (c < 2) {
         var cc = a - 2 + c;
@@ -148,23 +155,23 @@ while (a < 15) {
   a++;
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+var slide = 1;
+showSlides(slide);
+function plusSlides(d) {
+  showSlides((slide += d));
 }
-function currentSlide(n) {
-  showSlides((slideIndex = n));
+function currentSlide(d) {
+  showSlides((slide = d));
 }
-function showSlides(n) {
+function showSlides(d) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
+  if (d > slides.length) {
+    slide = 1;
   }
-  if (n < 1) {
-    slideIndex = slides.length;
+  if (d < 1) {
+    slide = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
@@ -172,6 +179,6 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  slides[slide - 1].style.display = "block";
+  dots[slide - 1].className += " active";
 }
