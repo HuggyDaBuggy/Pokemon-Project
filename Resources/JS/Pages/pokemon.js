4a866a114801787ca709e13c7a6f60bb1eeccab9
HuggyDaBuggy <hugo@maxtedfamily.net>
2020-10-06 09:15:02 +0800

    mage stuff

--- Resources/JS/Pages/pokemon.js
+++ Resources/JS/Pages/pokemon.js
@@ -31,38 +31,38 @@ var pokemons = [
   "Skuntank",
 ];
 var cutepokemon = [
-  ["Pikachu", "025", "The electic mouse pokémon. It stores electricity in it's cheeks and attacks with energy equivalent to a lighting bolt.", "", 3],
-  ["Eevee", "133", "The evolution pokémon. Eevee is most well known for the largest amount of possible evolutions - 8 in total.", "", 2],
+  ["Pikachu", "025", "The electic mouse pokémon. It stores electricity in it's cheeks and attacks with energy equivalent to a lighting bolt.", "", 3, ["", "", ""]],
+  ["Eevee", "133", "The evolution pokémon. Eevee is most well known for the largest amount of possible evolutions - 8 in total.", "", 2, ["", ""]],
   ["Mew", "151", "The new species pokémon. This mythical creature possesses the genetic information of every Pokémon and is rarely seen.", "", 1],
-  ["Togepi", "175", "The spike ball pokémon. It's shell is said to contain happiness and Togepi often shares it with others of pure intent.", "", 3],
-  ["Skitty", "300", "The kitten pokémon. Skitty likes to chase it's own tail until it falls dizzy. It is very popular as a pet.", "", 2],
-  ["Vulpix", "037", "The fox pokémon. It is able to control a flame in it's mouth that never goes out. As it grows, it's tails split to make more.", "", 2],
+  ["Togepi", "175", "The spike ball pokémon. It's shell is said to contain happiness and Togepi often shares it with others of pure intent.", "", 3, ["", "", ""]],
+  ["Skitty", "300", "The kitten pokémon. Skitty likes to chase it's own tail until it falls dizzy. It is very popular as a pet.", "", 2, ["", ""]],
+  ["Vulpix", "037", "The fox pokémon. It is able to control a flame in it's mouth that never goes out. As it grows, it's tails split to make more.", "", 2, ["", ""]],
   ["Emolga", "587", "", "", 1],
-  ["Deerling", "585", "", "", 2],
-  ["Snom", "872", "", "", 2],
-  ["Teddiursa", "216", "", "", 2],
+  ["Deerling", "585", "", "", 2, ["", ""]],
+  ["Snom", "872", "", "", 2, ["", ""]],
+  ["Teddiursa", "216", "", "", 2, ["", ""]],
   ["Jirachi", "385", "", "", 1],
-  ["jigglypuff", "039", "", "", 3],
+  ["Jigglypuff", "039", "", "", 3, ["", "", ""]],
   ["Shaymin", "492", "", "", 1],
   ["Meloetta", "648", "", "", 1],
   ["Absol", "359", "", "", 1],
 ];
 var uglypokemon = [
-  ["Jynx", "124", "", "", 2],
-  ["Probopass", "476", "", "", 2],
-  ["Garbodor", "569", "", "", 2],
-  ["Conkeldurr", "534", "", "", 3],
-  ["Barbaracle", "689", "", "", 2],
+  ["Jynx", "124", "", "", 2, ["", ""]],
+  ["Probopass", "476", "", "", 2, ["", ""]],
+  ["Garbodor", "569", "", "", 2, ["", ""]],
+  ["Conkeldurr", "534", "", "", 3, ["", "", ""]],
+  ["Barbaracle", "689", "", "", 2, ["", ""]],
   ["Stunfisk", "618", "", "", 1],
-  ["Seismitoad", "537", "", "", 3],
-  ["Purugly", "432", "", "", 2],
-  ["Muk", "089", "", "", 2],
-  ["Drowzee", "096", "", "", 2],
-  ["Swampert", "260", "", "", 3],
-  ["Walrein", "365", "", "", 3],
-  ["Malamar", "687", "", "", 2],
-  ["Noivern", "715", "", "", 2],
-  ["Skuntank", "435", "", "", 2],
+  ["Seismitoad", "537", "", "", 3, ["", "", ""]],
+  ["Purugly", "432", "", "", 2, ["", ""]],
+  ["Muk", "089", "", "", 2, ["", ""]],
+  ["Drowzee", "096", "", "", 2, ["", ""]],
+  ["Swampert", "260", "", "", 3, ["", "", ""]],
+  ["Walrein", "365", "", "", 3, ["", "", ""]],
+  ["Malamar", "687", "", "", 2, ["", ""]],
+  ["Noivern", "715", "", "", 2, ["", ""]],
+  ["Skuntank", "435", "", "", 2, ["", ""]],
 ];
 var pokemon = window.location.href.split("?")[1];
 var a = 0;
@@ -87,11 +87,22 @@ while (a < 15) {
     document.getElementById("related-pokemon").innerHTML = "Other Cute Pokemon";
     document.getElementById("pokemon-info").innerHTML = cutepokemon[a][2];
     document.getElementById("pokemon-stats").innerHTML = cutepokemon[a][3];
-    document.getElementById("pokemon-image-1").setAttribute("src", "Resources/Images/Pokemon/Cute/" + cutepokemon[a][1] + ".png");
+    document.getElementById("pokemon-image-1").setAttribute("src", "Resources/Images/Pokemon/Cute/" + pokemon + "/1.png");
     document.getElementById("pokemon-image-1").setAttribute("alt", pokemon);
     if (cutepokemon[a][4] == 1) {
       document.getElementById("pokemon-controls").style.display = "none";
       document.getElementById("pokemon-image-text-1").style.display = "none";
+    } else if (cutepokemon[a][4] == 2) {
+      document.getElementById("pokemon-image-2").setAttribute("src", "Resources/Images/Pokemon/Cute/" + pokemon + "/2.png");
+      document.getElementById("pokemon-image-2").setAttribute("alt", cutepokemon[a][5][1]);
+      document.getElementById("pokemon-image-text-3").innerHTML = cutepokemon[a][5][1];
+    } else if (cutepokemon[a][4] == 3) {
+      document.getElementById("pokemon-image-2").setAttribute("src", "Resources/Images/Pokemon/Cute/" + pokemon + "/2.png");
+      document.getElementById("pokemon-image-2").setAttribute("alt", cutepokemon[a][5][1]);
+      document.getElementById("pokemon-image-text-2").innerHTML = cutepokemon[a][5][1];
+      document.getElementById("pokemon-image-3").setAttribute("src", "Resources/Images/Pokemon/Cute/" + pokemon + "/3.png");
+      document.getElementById("pokemon-image-3").setAttribute("alt", cutepokemon[a][5][2]);
+      document.getElementById("pokemon-image-text-3").innerHTML = cutepokemon[a][5][2];
     }
     while (b < 4) {
       if (b < 2) {
@@ -126,51 +137,54 @@ while (a < 15) {
     document.getElementById("pokemon-image-1").setAttribute("alt", pokemon);
     if (uglypokemon[a][4] == 1) {
       document.getElementById("pokemon-controls").style.display = "none";
+      document.getElementById("pokemon-image-text-1").style.display = "none";
+    } else if (uglypokemon[a][4] == 2) {
+    } else if (uglypokemon[a][4] == 3) {
     }
-    while (c < 4) {
-      if (c < 2) {
-        var cc = a - 2 + c;
-        if (cc == -1) {
-          cc = 14;
+      while (c < 4) {
+        if (c < 2) {
+          var cc = a - 2 + c;
+          if (cc == -1) {
+            cc = 14;
+          }
+          if (cc == -2) {
+            cc = 13;
+          }
+        } else {
+          var cc = a - 1 + c;
+          if (cc == 15) {
+            cc = 0;
+          }
+          if (cc == 16) {
+            cc = 1;
+          }
         }
-        if (cc == -2) {
-          cc = 13;
-        }
-      } else {
-        var cc = a - 1 + c;
-        if (cc == 15) {
-          cc = 0;
-        }
-        if (cc == 16) {
-          cc = 1;
-        }
+        document.getElementById("related-pokemon-image-" + (c + 1)).setAttribute("src", "Resources/Images/Pokemon/Ugly/" + uglypokemon[cc][1] + ".png");
+        document.getElementById("related-pokemon-image-" + (c + 1)).setAttribute("alt", uglypokemon[cc][0]);
+        document.getElementById("related-pokemon-link-" + (c + 1)).setAttribute("href", "pokemon?" + uglypokemon[cc][0]);
+        document.getElementById("related-pokemon-" + (c + 1)).innerHTML = uglypokemon[cc][0];
+        c++;
       }
-      document.getElementById("related-pokemon-image-" + (c + 1)).setAttribute("src", "Resources/Images/Pokemon/Ugly/" + uglypokemon[cc][1] + ".png");
-      document.getElementById("related-pokemon-image-" + (c + 1)).setAttribute("alt", uglypokemon[cc][0]);
-      document.getElementById("related-pokemon-link-" + (c + 1)).setAttribute("href", "pokemon?" + uglypokemon[cc][0]);
-      document.getElementById("related-pokemon-" + (c + 1)).innerHTML = uglypokemon[cc][0];
-      c++;
-    }
   }
   a++;
 }
 
 var slide = 1;
 showSlides(slide);
-function plusSlides(d) {
-  showSlides((slide += d));
+function plusSlides(s) {
+  showSlides((slide += s));
 }
-function currentSlide(d) {
-  showSlides((slide = d));
+function currentSlide(s) {
+  showSlides((slide = s));
 }
-function showSlides(d) {
+function showSlides(s) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (d > slides.length) {
+  if (s > slides.length) {
     slide = 1;
   }
-  if (d < 1) {
+  if (s < 1) {
     slide = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
