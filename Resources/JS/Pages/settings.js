71d4321d3bc6653612291d29cf9a6806dea655ea
HuggyDaBuggy <hugo@maxtedfamily.net>
2020-10-07 18:38:58 +0800

    Most of js comments

--- Resources/JS/Pages/settings.js
+++ Resources/JS/Pages/settings.js
@@ -1,13 +1,16 @@
+//Sets the theme button correctly
 if (getStorage("Theme") == "Dark") {
   document.getElementById("theme-button").innerHTML = "&#9790;";
 } else {
   document.getElementById("theme-button").innerHTML = "&#9728;";
 }
+//Sets the autoscroll checkbox correctly
 if (getStorage("AutoScroll") == "On") {
   document.getElementById("auto-slideshow").checked = true;
 } else {
   document.getElementById("auto-slideshow").checked = false;
 }
+//CHanges the theme when theme button is clicked
 function themeChange() {
   if (getStorage("Theme") == "Dark") {
     setStorage("Theme", "Light");
@@ -19,6 +22,7 @@ function themeChange() {
     document.getElementById("theme-style").setAttribute("href", "Resources/CSS/Themes/dark.css");
   }
 }
+//Changes the auto slideshow setting when checkbox changes
 function slideshowChange() {
   var checkbox = document.getElementById("auto-slideshow");
   if (checkbox.checked) {
--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -1,3 +1,4 @@
+//Custom Storage Functions
 function setStorage(name, value) {
   if (typeof Storage !== "undefined") {
     return localStorage.setItem(name, value);
@@ -8,9 +9,11 @@ function getStorage(name) {
     return localStorage.getItem(name);
   }
 }
+//Capitalize Function
 function capitalize(input) {
   return input[0].toUpperCase() + input.slice(1);
 }
+//Disable Default Context Menu
 document.addEventListener(
   "contextmenu",
   function (rclick) {
@@ -18,13 +21,16 @@ document.addEventListener(
   },
   false
 );
+//Context Menu Function
 document.addEventListener("contextmenu", contextMenu);
+//Hide Context Menu when certain events happen
 window.onclick = function () {
   document.getElementById("context-menu").style.display = "none";
 };
 window.onscroll = function () {
   document.getElementById("context-menu").style.display = "none";
 };
+//Checks if it is the users first time on the site, then sets settings to defaults 
 if (getStorage("Theme") == null) {
   if (typeof Storage !== "undefined") {
     setStorage("Theme", "Dark");
@@ -33,11 +39,13 @@ if (getStorage("Theme") == null) {
     alert("Your browser does not support local storage, this will cause some features to not work correctly.");
   }
 }
+//Applies the custom theme
 if (getStorage("Theme") == "Light") {
   document.getElementById("theme-style").setAttribute("href", "Resources/CSS/Themes/light.css");
 } else {
   document.getElementById("theme-style").setAttribute("href", "Resources/CSS/Themes/dark.css");
 }
+//Shows the custom context menu on rclick - combined with line 25
 function contextMenu(event) {
   document.getElementById("context-menu").style.left = event.clientX - 17 + "px";
   document.getElementById("context-menu").style.top = event.clientY - 17 + "px";
