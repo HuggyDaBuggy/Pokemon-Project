7529ceef12f5b9e00a976bb57aa02d5a93c8ca3f
HuggyDaBuggy <hugo@maxtedfamily.net>
2020-10-11 20:46:01 +0800

    Fixed a dumb dumb

--- Resources/JS/Pages/pokemon.js
+++ Resources/JS/Pages/pokemon.js
@@ -328,8 +328,8 @@ function showSlides(s) {
   slides[slide - 1].style.display = "block";
   //Actives dot
   dots[slide - 1].className += " active";
-  //Checks if autoscroll is on
-  if (getStorage("AutoScroll")) {
+  //Checks if ImageScroll is on
+  if (getStorage("ImageScroll")) {
     //If on wait 2s then change slide by 1
     setTimeout(function () {
       showSlides((slide += 1));
--- Resources/JS/Pages/settings.js
+++ Resources/JS/Pages/settings.js
@@ -4,9 +4,12 @@ if (getStorage("Theme") == "Dark") {
 } else {
   document.getElementById("theme-button").innerHTML = "&#9728;";
 }
-//Sets the autoscroll checkbox correctly
-alert(getStorage("AutoScroll"));
-document.getElementById("auto-slideshow").checked = getStorage("AutoScroll");
+//Sets the ImageScroll checkbox correctly
+if (getStorage("ImageScroll") == "Enabled") {
+  document.getElementById("auto-slideshow").checked = true;
+} else {
+  document.getElementById("auto-slideshow").checked = false;
+}
 //CHanges the theme when theme button is clicked
 function themeChange() {
   if (getStorage("Theme") == "Dark") {
@@ -22,10 +25,8 @@ function themeChange() {
 //Changes the auto slideshow setting when checkbox changes
 function slideshowChange() {
   if (document.getElementById("auto-slideshow").checked) {
-    setStorage("AutoScroll", true);
-    alert(getStorage("AutoScroll"));
+    setStorage("ImageScroll", "Enabled");
   } else {
-    setStorage("AutoScroll", false);
-    alert(getStorage("AutoScroll"));
+    setStorage("ImageScroll", "Disabled");
   }
 }
