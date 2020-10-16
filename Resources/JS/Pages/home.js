e3e2789b941a721f7c783659cf8dc13be08411b3
Hugo Maxted <hugo@maxtedfamily.net>
2020-10-16 15:10:28 +0800

    Comment your mum
    am so funy

--- Resources/JS/Pages/home.js
+++ Resources/JS/Pages/home.js
@@ -1,3 +1,4 @@
+//Redirects you to the main website if you are using it elsewhere (for live preview it is dumb and stupid)
 if (window.location.href != "https://huggydabuggy.github.io/Pokemon-Project/") {
   window.location.replace("https://huggydabuggy.github.io/Pokemon-Project/");
 }
